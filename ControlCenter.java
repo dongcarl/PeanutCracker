38af1c918209c0218a29678ccbdbc8346391a117
Carl Dong <dongcarl@yahoo.co.uk>
2013-02-01 14:24:01 -0500

    First Commit
    All Classes Updated

---
+++ ControlCenter.java
@@ -0,0 +1,12 @@
+package PeanutCracker;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dongcarl
+ * Date: 2/1/13
+ * Time: 2:21 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ControlCenter
+{
+}
---
+++ Function.java
@@ -0,0 +1,12 @@
+package PeanutCracker;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dongcarl
+ * Date: 2/1/13
+ * Time: 2:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Function
+{
+}
