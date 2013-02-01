acbe7855eed0d371dbb6babcbb128d90e3f470a4
Carl Dong <dongcarl@yahoo.co.uk>
2013-02-01 15:48:20 -0500

    GraphDisplay Preliminary
    Preliminary code for GraphDisplay

--- GraphDisplay.java
+++ GraphDisplay.java
@@ -1,5 +1,10 @@
 package PeanutCracker;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+
 /**
  * Created with IntelliJ IDEA.
  * User: dongcarl
@@ -7,6 +12,32 @@ package PeanutCracker;
  * Time: 2:23 PM
  * To change this template use File | Settings | File Templates.
  */
-public class GraphDisplay
+public class GraphDisplay extends JPanel
 {
+
+	public static ArrayList<Point2D.Double> GraphArray;
+
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+
+		Graphics2D g2d = (Graphics2D) g;
+
+		for (int i = 0; i<GraphArray.size()-1; i++)
+		{
+			int x1 = (int)(GraphArray.get(i).x);
+			int y1 = (int)(GraphArray.get(i).y);
+
+			int x2 = (int)(GraphArray.get(i+1).x);
+			int y2 = (int)(GraphArray.get(i+1).y);
+
+			g2d.drawLine(x1, y1, x2, y2);
+		}
+	}
+
+	public GraphDisplay(ArrayList Input)
+	{
+		GraphArray = Input;
+	}
+
+
 }
