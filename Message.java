61992209bfb6ce7ffbdba07f01fc3dc8dadb38e0
William Baskin <mobile.wbaskin@gmail.com>
2013-02-17 16:19:40 -0500

    New Message class
    The message class now holds and object, and a string that may include
    operations to do on the object
    The Message also knows where it is from and to where it is going

--- Message.java
+++ Message.java
@@ -9,9 +9,41 @@ package PeanutCracker;
  */
 public class Message
 {
-<<<<<<< HEAD
-
-=======
+	Object main;
+	String message;
+	String destination;
+	String origin;
+	public Message(Object ObjectToPass, String StringToPass, String From, String To)
+	{
+		main = ObjectToPass;
+		message = StringToPass;
+		destination = To;
+		origin = From;
+	}
+	public Message getFull()
+	{
+		//return the message, possibly for copying
+		return this;
+	}
+	public Object getMain() 
+	{
+		//return the object passed, including an Event, Window or otherwise
+		return main;
+	}
+	public void setMain(Object main) 
+	{
+		//set the main object passed, including an Even, Window or otherwise
+		this.main = main;
+	}
+	public String getMessage() 
+	{
+		//get the message string, including which operation might be applied to a function, etc.
+		return message;
+	}
+	public void setMessage(String message) 
+	{
+		//set the message string, including which operation might be applied to a function, etc.
+		this.message = message;
+	}
 	
->>>>>>> mistake
 }
